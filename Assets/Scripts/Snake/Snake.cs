9bc159ec4a9f649d623e11f757ee86f48c9b9be7
johnklima <klima.de.sa@gmail.com>
2023-01-18 12:22:23 +0000

    Begin Project

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnikeMaister2000 : MonoBehaviour
+public class Snake : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
