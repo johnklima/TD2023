641e60bfd7bb1995b32f62d7a6a6bb082fef0a9b
Vegilli2 <canyohan@yahoo.no>
2023-01-19 00:14:26 +0100

    Added Snake and Snake package

---
+++ Assets/Scripts/Johan/SnakeScript.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class SnakeScript : MonoBehaviour
+{
+    public float moveSpeed = .1f;
+    public float turningSpeed = 180f;
+    public GameObject snakeBody;
+    public int gap = 280;
+    public float bodySpeed = 5f;
+    private List<GameObject> snakeBodyParts = new List<GameObject>();
+    private bool moving = false;
+    private List<Vector3> positionHistory = new List<Vector3>();
+    private float maxDistanceIndex = 5000;
+    public float sineWaveSpeed = 3.5f;
+    public float amplitude = 0.0001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        //Turning
+        float turningDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, turningDirection * turningSpeed * Time.deltaTime);
+        
+        //Store position history
+        if (moving)
+        {
+            positionHistory.Insert(0, transform.position);
+            //Wiggle
+            Sine(sineWaveSpeed, amplitude);
+        }
+        
+        // Move Body parts
+        int Index = 0;
+        foreach (var body in snakeBodyParts)
+        {
+            if (moving)
+            {
+                Vector3 point = positionHistory[Mathf.Min(Index * gap, positionHistory.Count - 1)];
+                Vector3 moveDirection = point - body.transform.position;
+               
+                body.transform.position += Vector3.MoveTowards(body.transform.position, moveDirection * bodySpeed * Time.deltaTime, maxDistanceIndex);
+                
+                body.transform.LookAt(point); 
+                
+                Index++;
+            }
+        }
+    }
+    //Add bodyparts to the snake
+    private void GrowSnake()
+    {
+        GameObject body = Instantiate(snakeBody, transform.position, transform.rotation);
+        snakeBodyParts.Add(body);
+    }
+    //Trigger GrowSnake
+     void OnTriggerEnter(Collider other) 
+     {
+        if (other.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+            GrowSnake();
+        }
+     }
+    //Sinewave movement
+     private void Sine(float speed, float Amplitude)
+     {
+         Vector3 pos = transform.position;
+         pos.x = Mathf.Sin(Time.time * speed) * Amplitude;
+         transform.position += transform.right * pos.x;
+     }
+}
