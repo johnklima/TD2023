53e0da36d5ff3124841f763bb521b93f94a752a6
johnklima <klima.de.sa@gmail.com>
2023-02-01 14:45:27 +0000

    full code version of footstep audio working!

--- Assets/Scripts/CharacterScripts/Player.cs
+++ Assets/Scripts/CharacterScripts/Player.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
 
 public class Player : MonoBehaviour
 {
@@ -11,9 +14,29 @@ public class Player : MonoBehaviour
 
     public HealthSystem healthSystem;
     [SerializeField] private HealthBar healthBar;
+       
+    
+    FMOD.Studio.EventInstance e;
+    FMOD.Studio.EventInstance ewalk;
+
+
+    public bool isInCombat = false;
+    public float  isOnPebbles = 0;
+    public float isOnForest = 0;
+    public float isOnDirt = 0;
+
+    private Vector3 prevPos;
 
     private void Awake()
     {
+        e = FMODUnity.RuntimeManager.CreateInstance("event:/Richard'sCave3Music_Attempt001");
+        e.start();
+       
+        ewalk = FMODUnity.RuntimeManager.CreateInstance("event:/Footstep");
+        ewalk.start();
+        ewalk.setPaused(true);
+
+
         Instance = this;
 
         healthSystem = new HealthSystem(maxHealth);
@@ -48,6 +71,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Vector3.Distance(transform.position, prevPos) > 0.02f)
+        {
+            
+            prevPos = transform.position;
+
+            
+            ewalk.setPaused(false);
+
+            /* //walking is 2d, no need for this.
+            FMOD.ATTRIBUTES_3D structure;
+            FMOD.VECTOR pos;
+
+            ewalk.get3DAttributes(out structure);
+
+            pos.x = transform.position.x;
+            pos.y = transform.position.y;
+            pos.z = transform.position.z;
+
+            structure.position = pos;
+
+            ewalk.set3DAttributes(structure);           
+            */
+
+            ewalk.setParameterByName("Dirt", isOnDirt);
+            ewalk.setParameterByName("Pebbles", isOnPebbles);
+            ewalk.setParameterByName("Forest", isOnForest);
+
+            
+
+        }
+        else
+        {
+            ewalk.setParameterByName("Dirt", 0);
+            ewalk.setParameterByName("Pebbles", 0);
+            ewalk.setParameterByName("Forest", 0);
+            ewalk.setPaused(true);
+        }
+
         if (Input.GetKeyDown(KeyCode.T))
         {
             healthSystem.DealDamage(10);
@@ -59,6 +121,17 @@ public class Player : MonoBehaviour
             healthSystem.GainHealth(10);
             Debug.Log(healthSystem.GetHealth());
         }
+
+        if(isInCombat)
+        {
+            e.setParameterByName("Combat", 1.0f);            
+        }
+        else 
+        {
+            e.setParameterByName("Combat", 0.0f);
+        }
+
+        
 
     }
 }
