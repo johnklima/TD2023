1e1a799e8321e8067d6d304dde764f06fdfda941
johnklima <klima.de.sa@gmail.com>
2023-02-08 11:04:50 +0000

    continue cannon

--- Assets/Scripts/John/AimCannon.cs
+++ Assets/Scripts/John/AimCannon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AimCannon : MonoBehaviour
 {
+
+    [SerializeField] Camera camera;
+    [SerializeField] Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class AimCannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Bit shift the index of the layer (7) to get a bit mask
+        int layerMask = 1 << 7; //enemy
+
+        Vector3 campos = camera.transform.position + camera.transform.forward;
+        Vector3 fwd = camera.transform.forward;
+        RaycastHit hit;
+        if (Physics.Raycast(campos, fwd, out hit, 300))
+        {
+            Debug.DrawRay(campos, fwd * hit.distance, Color.red);
+            target.position = hit.point;
+           
+        }
     }
 }
